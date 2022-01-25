9d831e691858499ee2c9e7aa55c2e96c3db1d9a9
Sebastien Delpeuch <sebastien.delpeuch@enseirb-matmeca.fr>
2022-01-25 14:27:49 +0100

    📝documentation(api): add documentation for api classes

--- api.py
+++ api.py
@@ -1,16 +1,16 @@
 """
-This file implements point endpoints to add/ delete and get a cartesian point from compliant mode or rviz
+This file implements endpoints for Reachy API allowing to control rechy mobile robot
 """
-import threading
 from flask import Blueprint, request, send_file
 from reachy_sdk import ReachySDK
-from werkzeug.exceptions import NotFound
 
 import reachy
 
 
 class ReachyAPI:
-
+    """
+    This class implements endpoints for Reachy API allowing to control rechy mobile robot
+    """
     def __init__(self):
         self.bp = Blueprint('reachy_api', __name__)
 
@@ -26,27 +26,61 @@ class ReachyAPI:
 
         self.reachy = ReachySDK(host='localhost')
 
-    def index(self):
-        return "Reachy API"
+    @staticmethod
+    def index():
+        """
+        METHOD : GET
+        ROUTE : /
+        BRIEF : Home  page for reachy API
+        TODO : improve home page
+        :return: {'status': "Success"}
+        """
+        return {'status': "Success"}, 200
 
     def play(self):
+        """
+        METHOD : POST
+        ROUTE : /play
+        PARAMETERS : {}
+        BRIEF : Play a tictactoe game (blocking function)
+        :return: {'status': "Success", 'data' : 'winner'} at the end of the game
+        """
         if request.method == 'POST':
-            # thread = threading.Thread(target=reachy.main, args=(self.reachy))
-            # thread.start()
             winner = reachy.main(self.reachy)
-            return {'status': "Success"}, 200
+            return {'status': "Success", 'data': winner}, 200
 
     def head_on(self):
+        """
+        METHOD : POST
+        ROUTE : /head/on
+        PARAMETERS : {}
+        BRIEF : Power on the head
+        :return: {'status': "Success"}
+        """
         if request.method == 'POST':
             self.reachy.turn_on('head')
             return {'status': "Success"}, 200
 
     def head_off(self):
+        """
+        METHOD : POST
+        ROUTE : /head/off
+        PARAMETERS : {}
+        BRIEF : Power off the head
+        :return: {'status': "Success"}
+        """
         if request.method == 'POST':
             self.reachy.turn_off('head')
         return {'status': "Success"}, 200
 
     def head_lookat(self):
+        """
+        METHOD : POST
+        ROUTE : /head/lookat
+        PARAMETERS : {'x': float, 'y': float, 'z': float, 'duration': float}
+        BRIEF : Look at a point in space with the head (blocking function)
+        :return: {'status': "Success"} at the end of the action
+        """
         if request.method == 'POST':
             data = request.get_json()
             x = float(data['x'])
@@ -57,23 +91,51 @@ class ReachyAPI:
             return {'status': "Success"}, 200
 
     def head_happy(self):
+        """
+        METHOD : POST
+        ROUTE : /head/happy
+        PARAMETERS : {}
+        BRIEF : Make the robot happy
+        :return: {'status': "Success"}
+        """
         if request.method == 'POST':
             reachy.happy_antennas(self.reachy)
             return {'status': "Success"}, 200
 
     def head_sad(self):
+        """
+        METHOD : POST
+        ROUTE : /head/sad
+        PARAMETERS : {}
+        BRIEF : Make the robot sad
+        :return: {'status': "Success"}
+        """
         if request.method == 'POST':
             reachy.sad_antennas(self.reachy)
             return {'status': "Success"}, 200
 
     def camera_left(self):
+        """
+        METHOD : GET
+        ROUTE : /camera/left
+        Take a picture from the left camera
+        :return: a flask response with the image
+        """
         if request.method == 'GET':
             image = self.reachy.left_camera.wait_for_new_frame()
-            return send_file(image, mimetype='image/jpeg')
+            image.save('left.jpg')
+            return send_file('left.jpg', mimetype='image/jpeg')
         return "Reachy API"
 
     def camera_right(self):
+        """
+        METHOD : GET
+        ROUTE : /camera/right
+        Take a picture from the right camera
+        :return: a flask response with the image
+        """
         if request.method == 'GET':
             image = self.reachy.right_camera.wait_for_new_frame()
-            return send_file(image, mimetype='image/jpeg')
+            image.save('right.jpg')
+            return send_file('right.jpg', mimetype='image/jpeg')
         return "Reachy API"
